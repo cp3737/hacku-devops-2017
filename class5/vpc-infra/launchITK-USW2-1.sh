aac47d1a615f5ad4823bb89fc72bb3de8b562c1f
Sonya Mollinger <sonya.mollinger@gmail.com>
2017-10-17 18:18:46 -0700

    Adding class 6 edits

--- class5/vpc-infra/launchITK-USW2-1.sh
+++ class5/vpc-infra/launchITK-USW2-1.sh
@@ -4,14 +4,22 @@
 #### Increment BASTION_ID to launch new box!  ####
 ### Otherwise script will behave idempotently ####
 ##################################################
+<<<<<<< HEAD
 export AWS_REGION="us-west-2"
 export ZONEA="us-west-2a"
 export ZONEB="us-west-2b"
 export ZONEC="us-west-2c"
+=======
+export AWS_REGION="us-east-1"
+export AWS_DEFAULT_REGION="us-east-1"
+export ZONEA="us-east-1a"
+export ZONEB="us-east-1b"
+export ZONEC="us-east-1c"
+>>>>>>> 3bad08e... Class6 changes
 
-export KEYPAIR="class-public"
+export KEYPAIR="hackor_user_private"
 export MGMT_IP="`curl -s icanhazip.com`/32"
 export MYGROUP="bastionSG"
-export MYSUBNET="subnet-fa396ea1"
+export MYSUBNET="subnet-e67765ca"
 
 /usr/bin/ansible-playbook ./bastion.yml
--- class6/launchCluster.sh
+++ class6/launchCluster.sh
@@ -4,18 +4,28 @@
 #### Increment BASTION_ID to launch new box!  ####
 ### Otherwise script will behave idempotently ####
 ##################################################
+<<<<<<< HEAD
 export BASTION_IDEMPOTENT_ID="03"
+=======
+export BASTION_IDEMPOTENT_ID="12"
+>>>>>>> 3bad08e... Class6 changes
 export PROVISION_STATE="present"
 
-export AWS_REGION="us-west-2"
-export ZONEA="us-west-2a"
-export ZONEB="us-west-2b"
-export ZONEC="us-west-2c"
+export AWS_REGION="us-east-1"
+export ZONEA="us-east-1a"
+export ZONEB="us-east-1b"
+export ZONEC="us-east-1c"
 
 export VPC_BASE="10.10"
-export KEYPAIR="class-public"
+export KEYPAIR="hackor_public"
 export MGMT_IP="`curl -s icanhazip.com`/32"
 
+<<<<<<< HEAD
 #/usr/local/bin/ansible-playbook -i ~/ansible/ec2.py  ./stage1.yml
 #/usr/local/bin/ansible-playbook -i ~/ansible/ec2.py  ./stage2.yml 
 /usr/local/bin/ansible-playbook -i ~/ansible/ec2.py  ./stage3.yml -vvv
+=======
+/usr/bin/ansible-playbook -i contrib/ec2.py ./stage1.yml 
+/usr/bin/ansible-playbook -i contrib/ec2.py ./stage2.yml
+/usr/bin/ansible-playbook -i contrib/ec2.py ./stage3.yml
+>>>>>>> 3bad08e... Class6 changes
