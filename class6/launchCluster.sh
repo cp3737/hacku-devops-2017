81c19563bf440e6a1103a6925faef1d95d844bb3
Sonya Mollinger <sonya.mollinger@gmail.com>
2017-10-22 12:05:51 -0700

    Class 7

--- class6/launchCluster.sh
+++ class6/launchCluster.sh
@@ -4,11 +4,7 @@
 #### Increment BASTION_ID to launch new box!  ####
 ### Otherwise script will behave idempotently ####
 ##################################################
-<<<<<<< HEAD
-export BASTION_IDEMPOTENT_ID="03"
-=======
-export BASTION_IDEMPOTENT_ID="12"
->>>>>>> 3bad08e... Class6 changes
+export BASTION_IDEMPOTENT_ID="13"
 export PROVISION_STATE="present"
 
 export AWS_REGION="us-east-1"
@@ -20,12 +16,6 @@ export VPC_BASE="10.10"
 export KEYPAIR="hackor_public"
 export MGMT_IP="`curl -s icanhazip.com`/32"
 
-<<<<<<< HEAD
-#/usr/local/bin/ansible-playbook -i ~/ansible/ec2.py  ./stage1.yml
-#/usr/local/bin/ansible-playbook -i ~/ansible/ec2.py  ./stage2.yml 
-/usr/local/bin/ansible-playbook -i ~/ansible/ec2.py  ./stage3.yml -vvv
-=======
-/usr/bin/ansible-playbook -i contrib/ec2.py ./stage1.yml 
-/usr/bin/ansible-playbook -i contrib/ec2.py ./stage2.yml
-/usr/bin/ansible-playbook -i contrib/ec2.py ./stage3.yml
->>>>>>> 3bad08e... Class6 changes
+#/usr/bin/ansible-playbook -i ~/ansible/ec2.py  ./stage1.yml
+#/usr/bin/ansible-playbook -i ~/ansible/ec2.py  ./stage2.yml 
+/usr/bin/ansible-playbook -i ~/ansible/ec2.py  ./stage3.yml -vvv
--- class7/destroyCluster.sh
+++ class7/destroyCluster.sh
@@ -7,16 +7,16 @@
 export CLUSTER_NAME="my-ecs-cluster"
 export PROVISION_STATE="absent"
 
-export AWS_REGION="us-west-2"
-export ZONEA="us-west-2a"
-export ZONEB="us-west-2b"
-export ZONEC="us-west-2c"
+export AWS_REGION="us-east-1"
+export ZONEA="us-east-1a"
+export ZONEB="us-east-1b"
+export ZONEC="us-east-1c"
 
-export KEYPAIR="xaas-or"
+export KEYPAIR="hackor_public"
 export MGMT_IP="`curl -s icanhazip.com`/32"
 
-/usr/local/bin/aws ec2 describe-security-groups --output text|grep SECURITYGROUPS| cut -f 3 > /tmp/groups
+/usr/bin/aws ec2 describe-security-groups --output text|grep SECURITYGROUPS| cut -f 3 > /tmp/groups
 
-/usr/local/bin/ansible-playbook -i contribl/ec2.py ./teardown.yml -vv
+/usr/bin/ansible-playbook -i contrib/ec2.py ./teardown.yml -vv
 
 #rm /tmp/groups
