2afa898ca632b19551c2d60252557c37ce9e7e16
Sonya Mollinger <sonya.mollinger@gmail.com>
2017-10-31 19:37:27 -0700

    more changes

---
+++ assign7/bin/env.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Setup Project Specfics - No Secrets
+export ECS_CLUSTER=assign7-hacko
+export ECS_PROJECT=assign7
+export ECS_PORT=8000
+export ECS_CLUSTER_SIZE=2
+export ECS_INSTANCE_TYPE=t2.small
+export AWS_REGION=us-west-2
+export AWS_KEY_PAIR=hackor_public_2
+export DOCKER_REPO=smcp42309
+export DOCKER_WEB_IMAGE=django-web
