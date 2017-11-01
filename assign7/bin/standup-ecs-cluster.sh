7aa1eb3b3fe49dca1007cb57de3de078071049b4
Sonya Mollinger <sonya.mollinger@gmail.com>
2017-10-31 19:37:00 -0700

    more changes

--- assign7/bin/standup-ecs-cluster.sh
+++ assign7/bin/standup-ecs-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ecs-cli up \
-  --keypair hackor_public\
+  --keypair hackor_public_2\
   --capability-iam \
   --size 1 \
-  --instance-type t2.micro  \
+  --instance-type t2.small  \
   --port 8000 \
